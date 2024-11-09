a8482455dba40392edeb0377e196fe5e53576fc9
Webnishimwe <hanurafiona@gmail.com>
2024-11-09 20:00:21 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
+
+  /*responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 570,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 'auto',
+        slidesToScroll: 'auto',
+        itemWidth: 280,
+        duration: 0.25
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25
+      }
+    }
+  ]
+
+
+
+
+
+
+
+
+
+
+/*
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    dots: '#dots',
+    draggable: true,
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  }); */
+
+
+
+
+
+
+
+
+
+
+
+/** 
+new Glider(document.querySelector(".glider"),{
+    slidesToshow: 3,
+    slidesToScroll: 2,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next',
+    }
+})*/
